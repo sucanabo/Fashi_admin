cd733af1bee7ce4ba0c253b2df262b1d5688f2d3
Phuc <sucanabo@gmail.com>
2020-05-07 21:05:05 +0700

    cat layout products

---
+++ pages/products.php
@@ -0,0 +1,12 @@
+<?php 
+    include_once ('../config.php');
+    $level = lv1;
+    $page_index = false;
+    $page_accounts = false;
+    $page_addproduct = false;
+    $page_editproduct = false;
+    $page_login = false;
+    $page_products = true;
+    include_once ($level.'layout.php');
+
+?>
---
+++ php/content/products-content.php
@@ -0,0 +1,11 @@
+<div class="container mt-5">
+    <div class="row tm-content-row">
+        <?php 
+            //product list
+            include_once ($level.php_path.comp_path.'products-list.php');
+            //product categories
+            include_once ($level.php_path.comp_path.'products-categories.php');
+        ?>
+        
+    </div>
+</div>
